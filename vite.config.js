b81c14175f1a2064adedf1e4939cfe6b8939d0d6
Jose Manuel Centeno Meza <josemcentenoc@gmail.com>
2023-12-24 14:46:39 -0300

    Preparado para github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://JoseMcmW.github.io/app-melp/'
+  proxy: {
+    '/api': {
+      target: 'https://recruiting-datasets.s3.us-east-2.amazonaws.com',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
+    },
+  },
+  base: '/app-melp/'
 })
