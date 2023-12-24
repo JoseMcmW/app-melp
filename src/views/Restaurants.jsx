b51c6a3f9b25c96d77e45678484ff6468fd61b4a
Jose Manuel Centeno Meza <josemcentenoc@gmail.com>
2023-12-24 15:53:42 -0300

    orden alfabetico

--- src/views/Restaurants.jsx
+++ src/views/Restaurants.jsx
@@ -1,18 +1,24 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from "react";
-import { Container, Typography, Grid, Pagination } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Grid,
+  Pagination,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import SimpleBackdrop from "../components/SimpleBackdrop";
 import Modal from "../components/Modal";
 import { useRestaurant } from "../Context/restaurantContext";
 import RestaurantCards from "./RestaurantCards";
 import Restaurant from "./Restaurant";
 
-// eslint-disable-next-line react/prop-types
 const Restaurants = () => {
   const { restaurants, loading, getRestaurants } = useRestaurant();
   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
   const [openModal, setOpenModal] = useState(false);
   const [page, setPage] = useState(1);
+  const [sortOption, setSortOption] = useState("default"); // Agregado para el ordenamiento
   const itemsPerPage = 12;
 
   const handleOpenModal = (restaurant) => {
@@ -33,12 +39,26 @@ const Restaurants = () => {
     getData();
   }, []);
 
+  const sortedRestaurants = restaurants && restaurants.slice(); // Clonar el array para no afectar el original
+
+
+  if (sortOption === "alphabetical") {
+    // Ordenar alfabéticamente por el nombre del restaurante
+    sortedRestaurants.sort((a, b) =>
+      a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
+    );
+  }
+
   const startIndex = (page - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const currentRestaurants = restaurants?.slice(startIndex, endIndex);
+  const currentRestaurants = sortedRestaurants?.slice(startIndex, endIndex);
 
   const handlePageChange = (event, newPage) => {
     setPage(newPage);
+  };
+
+  const handleSortChange = (event) => {
+    setSortOption(event.target.value);
   };
 
   return (
@@ -47,9 +67,19 @@ const Restaurants = () => {
         Restaurantes
       </Typography>
       <Typography variant="body2" mt={"1.5rem"} mb={"1.875rem"}>
-        Selecciona los que mas te gusten y agregalo a tus favoritos para que
+        Selecciona los que más te gusten y agrégalo a tus favoritos para que
         puedas elegir la mejor opción dentro de tus favoritos.
       </Typography>
+      {/* Agregar el componente Select para el ordenamiento */}
+      <Select
+        label="Ordenar por"
+        value={sortOption}
+        onChange={handleSortChange}
+        sx={{ marginBottom: 2 }}
+      >
+        <MenuItem value="default">Sin orden</MenuItem>
+        <MenuItem value="alphabetical">Alfabéticamente</MenuItem>
+      </Select>
       <Grid container spacing={2}>
         <RestaurantCards
           restaurants={currentRestaurants}
@@ -57,7 +87,7 @@ const Restaurants = () => {
         />
       </Grid>
       <Pagination
-        count={Math.ceil(restaurants?.length / itemsPerPage)}
+        count={Math.ceil(sortedRestaurants?.length / itemsPerPage)}
         page={page}
         onChange={handlePageChange}
         color="primary"
